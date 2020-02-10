2c2a5195c274e50ff5ecb181a17cf35a22bdc41a
MARCOS SOUZA CRUZ JUNIOR <marcos.scjunior@senacsp.edu.br>
2020-02-10 13:44:47 -0300

    Primeira aula POO

---
+++ bancodigital/BancoDigital/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDigital
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int conta;
+        public double saldo = 100;
+    }
+}
---
+++ bancodigital/BancoDigital/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDigital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variável do tipo ContaCorrente com a instância da classe
+            ContaCorrente contaDoJoao = new ContaCorrente();
+            contaDoJoao.titular = "João";
+            contaDoJoao.agencia = 2424;
+            contaDoJoao.conta = 001222;
+            contaDoJoao.saldo += 200;
+         
+
+            Console.WriteLine("Titular: " + contaDoJoao.titular);
+            Console.WriteLine("Agência: " + contaDoJoao.agencia);
+            Console.WriteLine("conta: " + contaDoJoao.conta);
+            Console.WriteLine("saldo: " + contaDoJoao.saldo);
+            
+            Console.ReadLine();
+        }
+    }
+}
