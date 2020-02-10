f01ae78bf0c7681a6fe9e70f994b34a29aaf8d93
MARCOS SOUZA CRUZ JUNIOR <marcos.scjunior@senacsp.edu.br>
2020-02-10 16:18:13 -0300

    Segunda Aula

--- bancodigital/BancoDigital/ContaCorrente.cs
+++ bancodigital/BancoDigital/ContaCorrente.cs
@@ -10,7 +10,57 @@ namespace BancoDigital
     {
         public string titular;
         public int agencia;
-        public int conta;
-        public double saldo = 100;
+        public int conta; // numero;
+        public double saldo;
+
+        //Função Sacar 
+                    public bool Sacar(double valor)
+            {
+                if (this.saldo >= valor)
+                {
+                    this.saldo -= valor;
+	       Console.WriteLine("Saque realizado com sucesso.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Função não permitida, saldo insuficiente");
+                    return false;   
+                }
+
     }
+
+       //FUNÇÃO DEPOSITAR (SEM RETORNO)
+       public void Depositar(double valor)
+       	 {
+            		Console.WriteLine("Depósito realizado COM SUCESSO.");
+           		 this.saldo += valor;
+        }
+
+
+        //FUNÇÃO TRANSFERENCIA
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            	if (this.saldo >= valor)
+           	{
+            this.saldo -= valor;
+        	//contaDestino.saldo += valor;
+	    // Mas como fizemos o método Depositar, podemos usá-lo em vez
+	    // de lidar com a lógica de acessar o campo saldo da classe.	
+	        contaDestino.Depositar(valor);
+    	Console.WriteLine("Transferência realizado com sucesso");
+	    return true;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insufiente para Transferência R$ " +saldo);
+         return false;            
 }
+        }
+
+
+    }
+
+
+}
+    
--- bancodigital/BancoDigital/Program.cs
+++ bancodigital/BancoDigital/Program.cs
@@ -15,14 +15,42 @@ namespace BancoDigital
             contaDoJoao.titular = "João";
             contaDoJoao.agencia = 2424;
             contaDoJoao.conta = 001222;
-            contaDoJoao.saldo += 200;
+            contaDoJoao.saldo += 100;
          
 
             Console.WriteLine("Titular: " + contaDoJoao.titular);
             Console.WriteLine("Agência: " + contaDoJoao.agencia);
-            Console.WriteLine("conta: " + contaDoJoao.conta);
+            Console.WriteLine("conta: " + contaDoJoao.conta); // numero;
             Console.WriteLine("saldo: " + contaDoJoao.saldo);
             
+            //função Sacar
+            contaDoJoao.Sacar(40);
+            Console.WriteLine("Saldo após o saque é de R$ " + contaDoJoao.saldo);
+
+            //Função depositar
+            contaDoJoao.Depositar(500);
+            Console.WriteLine("Saldo após o depósito é de R$ " + contaDoJoao.saldo);
+
+            
+            //programa  uma conta nova para transferência
+            ContaCorrente contaDaMaria = new ContaCorrente();
+            contaDaMaria.titular = "Maria";
+            Console.WriteLine("O saldo da conta da Maria é R$ " + contaDaMaria.saldo);
+
+
+            //o código para transferir dinheiro a conta nova:
+            contaDoJoao.Transferir(540, contaDaMaria);
+            Console.WriteLine("O saldo da conta do João agora é R$ " + contaDoJoao.saldo);
+            Console.WriteLine("O saldo da conta da Maria agora é R$ " + contaDaMaria.saldo);
+
+
+            //Apenas para teste, guarde o resultado da transferência em uma variável booleana e depois verifique se o resultado foi True ou False:
+            bool resultadoTransferencia = contaDoJoao.Transferir(540, contaDaMaria);
+            Console.WriteLine("Resultado da transferência é " + resultadoTransferencia);
+
+
+    
+
             Console.ReadLine();
         }
     }
